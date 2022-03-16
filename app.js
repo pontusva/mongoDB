bd4b407b108d62cc14207273738fce2a8eecd54a
Pontus <pontus00valdes@gmail.com>
2022-03-16 11:27:08 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+require('dotenv/config');
+
+app.use(bodyParser.json());
+
+// import routes
+const postsRoute = require('./routes/posts');
+
+app.use('/posts', postsRoute);
+
+
+// Routes
+app.get('/', (req, res) => {
+    res.send('We Are On Home');
+});
+
+
+
+// connect to DB
+mongoose.connect(process.env.DB_CONNECTION, () => {
+    console.log("connected do DB c:")
+});
+
+
+// listening
+app.listen(process.env.PORT || 3000,
+    () => console.log("server is running..."));
---
+++ models/Post.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const PostSchema = mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+
+    description: {
+        type: String,
+        required: true
+    }
+});
+
+
+module.exports = mongoose.model('Posts', PostSchema);
