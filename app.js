aa33a797acf5e0ebb8a31f4a61ee1146088d964b
Pontus <pontus00valdes@gmail.com>
2022-03-21 10:12:44 +0100

    changed port

--- app.js
+++ app.js
@@ -29,5 +29,5 @@ mongoose.connect(process.env.DB_CONNECTION, () => {
 
 
 // listening
-app.listen(process.env.PORT || 5000,
+app.listen(process.env.PORT || '8080',
     () => console.log("server is running..."));
