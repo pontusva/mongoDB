ffc77457018a8a316e2afff9c57e289b3ab4d6e6
Pontus <pontus00valdes@gmail.com>
2022-03-21 10:23:52 +0100

    updated dotenv

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const PORT = process.env.PORT || 8080;
-require('dotenv/config');
+require('dotenv').config();
 
 // Middlewares
 app.use(cors());
