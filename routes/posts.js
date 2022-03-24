99868e6ea5cb02b2988bed6a537f504f78337b14
Pontus <pontus00valdes@gmail.com>
2022-03-24 09:22:51 +0100

    removed /

--- routes/posts.js
+++ routes/posts.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const Post = require('../models/Post');
-const MilitaryPost = require('../models/MilitaryPost/');
+const MilitaryPost = require('../models/MilitaryPost');
 
 
 // gets all the posts
